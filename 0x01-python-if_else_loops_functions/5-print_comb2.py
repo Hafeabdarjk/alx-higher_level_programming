14d1c76df1d17e218024228b29788fe18744822e
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-24 08:30:13 +0300

    05

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i < 10:
+        number = "0{}, ".format(i)
+    elif i < 99:
+        number = "{}, ".format(i)
+    else:
+         number = "{}".format(i)
+    if i < 99:
+        print(number, end='')
+    else:
+        print(number)
