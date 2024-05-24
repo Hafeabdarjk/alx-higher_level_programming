07b834c301b50bfe1f4f9951b5c9365854444348
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-24 09:13:35 +0300

    06

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(9):
+    n = i+1
+    for j in range (n,10):
+        number = "{}{}, ".format(i,j)
+        last_number = "{}{}".format(i,j)
+        if i == 8 and j == 9:
+            print(last_number)
+        else:
+            print(number, end='')
