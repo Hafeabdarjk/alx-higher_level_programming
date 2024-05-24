2a2d11a95a267009e55ad74db237564f3ec48621
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-24 09:16:02 +0300

    06

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for i in range(9):
     n = i+1
     for j in range(n, 10):
-        number = "{}{}, ".format(i,j)
-        last_number = "{}{}".format(i,j)
+        number = "{}{}, ".format(i, j)
+        last_number = "{}{}".format(i, j)
         if i == 8 and j == 9:
             print(last_number)
         else:
