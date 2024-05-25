809d3f7e668d03e7f2a15e4ea11f355e27c6fa04
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-25 20:36:30 +0300

    0-0-0-0-0-0-0-0-0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     for i in range(len(my_string)-1):
         chrc = my_string[i]
-        if i == 'c' or "C":
+        if chrc == 'c' or "C":
             continue
         elif i == len(my_string) - 1:
             print(chrc)
