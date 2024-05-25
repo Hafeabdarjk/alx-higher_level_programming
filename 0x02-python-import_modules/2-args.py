b5ae67f589d0fefa76a3cf2d5c001490b7923867
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-25 09:22:31 +0300

    0000

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    argn = len(argv) - 1
-    if len(argv) == 0:
+    if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
