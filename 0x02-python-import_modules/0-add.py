e4de989b7fab4f72bd6ca3cc5db59866fb49c0de
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-25 08:10:35 +0300

    0000

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from add_0 import add
+print("{} + {} = {}".format(add.a, add.b, add(1, 2)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 1
+    b = 2
+    return a + b
+
+
+def main():
+    pass
+if "{}".format(__name__) == "__main__":
+    main ()
