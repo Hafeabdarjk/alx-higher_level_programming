77fa1571b715912cd54e1d4daad13a915a3177e2
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-25 20:56:55 +0300

    0-0-0-0-0-0-0-0-0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)-1):
-        chrc = my_string[i]
-        if chrc == 'c' or "C":
-            continue
-        elif i == len(my_string) - 1:
-            print(chrc)
-        else:
-            print(chrc, end='')
+    new_string = [x for x in my_string if x != 'c']
+    newer_string = [x for x in new_string if x != 'C']
+    for i in newer_string:
+            print(i, end = '')
+    print()
