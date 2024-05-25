9f595a4cc2657acd78f11c2b23e1846303579457
hafeabdarjk <apdohafez1256@gmail.com>
2024-05-25 21:04:11 +0300

    0-0-0-0-0-0-0-0-0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [x for x in my_string if x != 'c']
-    newer_string = [x for x in new_string if x != 'C']
-    for i in newer_string:
-            print(i, end = '')
-    print()
+    c_list = [x for x in my_string if x != 'c']
+    C_list = [x for x in c_list if x != 'C']
+    new_string = ""
+    for i in C_list:
+        new_string = new_string + i
+    return new_string
