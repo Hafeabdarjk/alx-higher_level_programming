7c83f755b160f22afd9036ba53e7a1b25475823c
hafeabdarjk <apdohafez1256@gmail.com>
2024-06-03 12:57:31 +0300

    0-0-0-0-0-0-0-0-0

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] / 2
+        a_dictionary[key] = a_dictionary[key] * 2
         new_dictionary.update({key: a_dictionary[key]})
     return new_dictionary
