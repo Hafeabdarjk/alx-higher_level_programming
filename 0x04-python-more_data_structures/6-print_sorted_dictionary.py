d5f037732e6b777d37a4fea9c9d24b869e27dd2b
hafeabdarjk <apdohafez1256@gmail.com>
2024-06-03 12:32:04 +0300

    0-0-0-0-0-0-0-0-0

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,7 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     keys_list = list(a_dictionary.keys())
     keys_list.sort()
-    sorted_dict = {}
     for key in keys_list:
         for x in a_dictionary:
             if x == key:
