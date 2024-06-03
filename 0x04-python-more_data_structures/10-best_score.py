2a2a2978d9699f58ddc4a9a6ad0e4b692df6abfa
hafeabdarjk <apdohafez1256@gmail.com>
2024-06-03 14:09:07 +0300

    0-0-0-0-0-0-0-0-0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None  or a_dictionary == {}:
+    if a_dictionary == None or a_dictionary == {}:
         return None
     biggest_value = max(a_dictionary.values())
     for key in a_dictionary:
