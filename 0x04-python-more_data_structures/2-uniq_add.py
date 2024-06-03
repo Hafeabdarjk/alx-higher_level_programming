063eb4d0ce6f4f01657afb84623f26fed8d2e9e9
hafeabdarjk <apdohafez1256@gmail.com>
2024-06-03 10:31:07 +0300

    0-0-0-0-0-0-0-0-0

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_tuple = tuple(my_list)
+    new_set = set(my_list)
     sum = 0
-    for x in new_tuple:
+    for x in new_set:
         sum += x
     return sum
