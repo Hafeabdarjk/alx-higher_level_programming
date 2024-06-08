687df40ba8a59466e14980cfd9ece138bfbe5e50
hafeabdarjk <apdohafez1256@gmail.com>
2024-06-08 14:02:40 +0300

    0-0-0-0-0-0-0-0-0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,11 +11,8 @@ class Square:
         Args:
             size (int): the size of an instance.
         """
-
-        if size is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-            self.__size = size
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
