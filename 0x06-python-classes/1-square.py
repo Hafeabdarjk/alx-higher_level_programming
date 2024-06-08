3cfa35e259061095d9d0a810855babece4cb1622
hafeabdarjk <apdohafez1256@gmail.com>
2024-06-08 13:39:11 +0300

    0-0-0-0-0-0-0-0-0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define an empty class called square."""
+
+
+class Square:
+    """This is the square class."""
+
+    def __init__(self, size):
+        """initializing an instance of a scuare
+        
+        Args:
+            size (int): the size of an instance 
+        """
+        self.__size = size
